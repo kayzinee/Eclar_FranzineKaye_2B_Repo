d943ac3b6b6f983db03a47e431f89e881f51c246
kayzinee <franzinekayedeclar@gmail.com>
2025-03-21 14:08:23 -1100

    Getting Functional

---
+++ Getting Functional/function.js
@@ -0,0 +1,44 @@
+// var dogName = "rover";
+// var dogWeight = 23;
+// if (dogWeight > 20) {
+//     console.log (dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// var dogName = "spot";
+// var dogWeight = 13;
+// if (dogWeight > 20) {
+//     console.log (dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// var dogName = "jin";
+// var dogWeight = 15;
+// if (dogWeight > 20) {
+//     console.log (dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// var dogName = "polly";
+// var dogWeight = 30;
+// if (dogWeight > 20) {
+//     console.log (dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+function bark(name, weight){
+    if (weight > 20) {
+        console.log (name + " says WOOF WOOF");
+    } else {
+        console.log(name + " says woof woof");
+    }
+}
+
+bark("Hazel", 23);
+bark("Kaye", 19);
+bark("Mary", 25);
+bark("Kyle", 15);
