af8a1c74a24f0e903440fd3aee022646e0f4c5aa
kayzinee <franzinekayedeclar@gmail.com>
2025-03-24 11:22:53 +0800

    Function-Activity

---
+++ Activities/Function-Activity/script.js
@@ -0,0 +1,13 @@
+function whatShallIWear(temp){
+    if (temp <60) {
+        console.log ("wear a jacket");
+    } else if (temp < 70) {
+        console.log ("wear a sweater");
+    } else {
+        console.log ("wear t-shirt");
+    }
+}
+
+whatShallIWear(50);
+whatShallIWear(60);
+whatShallIWear(80);
